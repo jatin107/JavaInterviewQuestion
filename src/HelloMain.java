fd22d1f48d25998cdffdc6162c7f7d55e646c79b
jatin107 <jatingoyal100@gmail.com>
2017-05-06 08:35:43 +0530

    Initial commit

---
+++ src/HelloMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jatin on 5/6/2017.
+ */
+public class HelloMain {
+
+    public static void main(String[] args) {
+        System.out.println(" Welcome to Java Interview Question");
+    }
+}
