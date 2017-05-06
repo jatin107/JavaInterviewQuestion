2913bd284dd594ae70d9fd48f64f8b63088bc09c
jatin107 <jatingoyal100@gmail.com>
2017-05-06 08:46:44 +0530

    ok

--- src/HelloMain.java
+++ src/HelloMain.java
@@ -6,7 +6,8 @@ public class HelloMain {
     public static void main(String[] args) {
         System.out.println(" Welcome to Java Interview Question :)");
         System.out.println(" Welcome to Java Interview Question :)");
-
+        System.out.println(" Welcome to Java Interview Question :)");
 
     }
 }
+
