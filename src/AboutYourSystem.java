0ad4690002a9abff58230c048ae24aed8961a365
jatin107 <jatingoyal100@gmail.com>
2018-01-20 11:13:21 +0530

    Cracking the Coding interview Question 1.6  String Compression: Implement a method to perform basic string compression using the counts  of repeated characters. For example, the string aabcccccaaa would become a2blc5a3 . If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a - z).
    Signed-off-by: jatin107 <jatingoyal100@gmail.com>

---
+++ src/AboutYourSystem.java
@@ -0,0 +1,22 @@
+import java.util.Properties;
+
+/**
+ * Created by jatin on 5/6/2017.
+ */
+public class AboutYourSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Interview Question :)");
+        System.out.println("Java Version:" + System.getProperty("java.version"));
+        System.out.println("AvailableProcessors:" + Runtime.getRuntime().availableProcessors());
+        System.out.printf("MaxMemory  :%.3fGiB\n", +((double) Runtime.getRuntime().maxMemory() / (1024.0 * 1024.0 * 1024.0)));
+        System.out.printf("TotalMemory:%.3fGiB\n", +(Runtime.getRuntime().totalMemory() / (1024.0 * 1024.0 * 1024.0)));
+        System.out.printf("FreeMemory :%.3fGiB\n", +(Runtime.getRuntime().freeMemory() / (1024.0 * 1024.0 * 1024.0)));
+        System.out.printf("Used Memory:%.3fGiB\n", +((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024.0 * 1024.0 * 1024.0)));
+        Properties props = System.getProperties();
+        props.list(System.out);
+
+    }
+}
+
+
