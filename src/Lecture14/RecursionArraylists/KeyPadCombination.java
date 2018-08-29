05c90d8a8c00b96468cf8ec95d076bd112192b4f
Mr. X <Mr. X@192.168.1.75>
2018-08-29 08:39:15 +0530

    ok

---
+++ src/Lecture14/RecursionArraylists/KeyPadCombination.java
@@ -0,0 +1,38 @@
+package Lecture14.RecursionArraylists;
+
+import java.util.ArrayList;
+
+public class KeyPadCombination {
+
+	static String[] code = { ".", "abc", "def", "ghi", "jkl", "mno", "pqr", "st", "uvwx", "yz" };
+
+	public static ArrayList<String> getKPC(String str) {
+
+		if (str.length() == 0) {
+			ArrayList<String> al = new ArrayList<>();
+			al.add("");
+			return al;
+
+		}
+		char ch = str.charAt(0);
+		int chint = (int) ch;
+		String ros = str.substring(1);
+
+		ArrayList<String> ral = getKPC(ros);
+		ArrayList<String> newal = new ArrayList<>();
+
+		for (String s : ral) {
+			for (char codess : code[ch - '0'].toCharArray())
+				newal.add(codess + s);
+		}
+
+		return newal;
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(getKPC("197"));
+	}
+
+}
---
+++ src/Lecture14/RecursionArraylists/SubStringArrayList.java
@@ -0,0 +1,61 @@
+package Lecture14.RecursionArraylists;
+
+import java.util.ArrayList;
+
+public class SubStringArrayList {
+
+	public static ArrayList<String> subSequence(String str) {
+
+		if (str.length() == 0) {
+			ArrayList<String> al = new ArrayList<>();
+			al.add("");
+			return al;
+
+		}
+		char ch = str.charAt(0);
+		String ros = str.substring(1);
+
+		ArrayList<String> ral = subSequence(ros);
+		ArrayList<String> newal = new ArrayList<>();
+
+		for (String s : ral) {
+			newal.add(s);
+			newal.add(ch + s);
+		}
+
+		return newal;
+
+	}
+
+	public static ArrayList<String> subSequenceWithASCII(String str) {
+
+		if (str.length() == 0) {
+			ArrayList<String> al = new ArrayList<>();
+			al.add("");
+			return al;
+
+		}
+		char ch = str.charAt(0);
+		int chint = (int) ch;
+		String ros = str.substring(1);
+
+		ArrayList<String> ral = subSequenceWithASCII(ros);
+		ArrayList<String> newal = new ArrayList<>();
+
+		for (String s : ral) {
+			newal.add(s);
+			newal.add(ch + s);
+			// newal.add(ch + s);
+			newal.add((int) chint + s);
+		}
+
+		return newal;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(subSequence("abc"));
+		System.out.println(subSequenceWithASCII("abc"));
+	}
+
+}
